a9d46043c1e007012c3ea59d8e0eb66129426004
EternalCoders <34636947+EternalCoderss@users.noreply.github.com>
2023-10-04 13:33:27 +0530

    Time: 35 ms (13.37%), Space: 26.2 MB (52.23%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -3,9 +3,11 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
 
+        //run loop for n and sqrt of nums[i]
         for(int i = 0; i < n; i++){
             nums[i] = nums[i]* nums[i];
         }
+        //sort function for vector
         sort(nums.begin(), nums.end());
         return nums;
     }
