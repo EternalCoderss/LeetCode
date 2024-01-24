d349c194881e7baac1bfdfb1dc4ea8618595311e
EternalCoders <34636947+EternalCoderss@users.noreply.github.com>
2024-01-24 15:08:38 +0530

    Time: 29 ms (19.46%) | Memory: 27.3 MB (5.69%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k%n;
-
-        reverse(nums.begin(), nums.end());
+        k %= nums.size(); //To check if k is not greater thn array.
+        //thn
+        reverse(nums.begin() , nums.end());
         reverse(nums.begin(), nums.begin()+k);
         reverse(nums.begin()+k, nums.end());
-
+        
     }
 };
