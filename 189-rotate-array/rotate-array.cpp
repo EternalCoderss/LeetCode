91de53dcba44420410833e947f71977ec9431978
EternalCoders <34636947+EternalCoderss@users.noreply.github.com>
2024-01-24 15:09:40 +0530

    Time: 26 ms (36.52%) | Memory: 27.3 MB (5.69%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,8 +4,11 @@ public:
         k %= nums.size(); //To check if k is not greater thn array.
         //thn
         reverse(nums.begin() , nums.end());
+        //full array reverse kia
         reverse(nums.begin(), nums.begin()+k);
+        //k tak reverse kia..
         reverse(nums.begin()+k, nums.end());
+        //k k bad (n-k) end tk reverse kia..
         
     }
 };
