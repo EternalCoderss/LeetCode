eb03a040cacad0606fc627b225439e7a7cc5c9d2
EternalCoders <34636947+EternalCoderss@users.noreply.github.com>
2025-11-24 14:37:07 +0530

    Time: 0 ms (100%), Space: 17.8 MB (58.74%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         triangle = []
+
         for row in range(numRows):
-            new_row = [] # create new blank cols
-            for col in range(row + 1):
+            new_row = []
+            for col in range(row+1):
                 if col == 0 or col == row:
                     new_row.append(1)
                 else:
                     new_row.append(triangle[row-1][col-1] + triangle[row-1][col])
             triangle.append(new_row)
-        return triangle
-        
+        return triangle           
 
             
         
